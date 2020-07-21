ec2b1f7e7263e4e8bc7409c064b64b77f502f778
Eealvarez39 <68433382+Eealvarez39@users.noreply.github.com>
2020-07-21 14:00:25 -0500

    Six buttons change text

--- DefendBase10/Assets/firstButton.cs
+++ DefendBase10/Assets/firstButton.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class firstButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void SetText(string text)
+	{
+		Text txt = transform.Find("Text").GetComponent<Text>();
+		txt.text = text;
+	}
 }
