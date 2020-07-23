96ee53e5804ed005ea30eceef16e729a7cb04064
Eealvarez39 <68433382+Eealvarez39@users.noreply.github.com>
2020-07-23 16:21:54 -0500

    Buttons sort of work

---
+++ DefendBase10/Assets/ToggleButtonManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ToggleButtonManager : MonoBehaviour
+{
+    public Toggley[] buttons;
+    public int buttonValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Toggley btn in buttons)
+	{
+		buttonValue = btn.value * (int)Math.Pow(2, btn.digitNum);
+	}
+    }
+}
--- DefendBase10/Assets/Toggley.cs
+++ DefendBase10/Assets/Toggley.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class Toggley : MonoBehaviour
 {
+	[SerializeField]
+	public int digitNum = 0;
 	public GameObject switchOn, switchOff;
-	int firstNumber = 0;
+	public int value = 0;
 
 	void Start(){
 		OnChangeValue();
 	}
 	void Update(){
-		Debug.Log(firstNumber);
+		//Debug.Log(firstNumber);
 	}
 	public void OnChangeValue() {
 		bool onoffSwitch = gameObject.GetComponent<Toggle>().isOn;
@@ -20,13 +22,13 @@ public class Toggley : MonoBehaviour
 		{
 			switchOn.SetActive(true);
 			switchOff.SetActive(false);
-			firstNumber = 1;
+			value = 1;
 		}
 		if(!onoffSwitch)
 		{
 			switchOn.SetActive(false);
 			switchOff.SetActive(true);
-			firstNumber = 0;
+			value = 0;
 
 		}
 	}
