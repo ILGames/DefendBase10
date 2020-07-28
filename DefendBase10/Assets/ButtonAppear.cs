8f7a51e3c0c6e771e596d0e41a9ec72c3810ea29
Eealvarez39 <68433382+Eealvarez39@users.noreply.github.com>
2020-07-27 19:07:31 -0500

    Starting the Dialogue boxes

--- DefendBase10/Assets/ButtonAppear.cs
+++ DefendBase10/Assets/ButtonAppear.cs
@@ -8,68 +8,82 @@ public class ButtonAppear : MonoBehaviour{
 
 	public Toggle toggle1, toggle2, toggle3, toggle4, toggle5, toggle6;
 	public int levelPassed;
+	GameObject Button_2 = GameObject.Find("Button_2");
+	GameObject Button_3 = GameObject.Find("Button_3");
+	GameObject Button_4 = GameObject.Find("Button_4");
+	GameObject Button_5 = GameObject.Find("Button_5");
+	GameObject Button_6 = GameObject.Find("Button_6");
 	void start() 
 	{
 		levelPassed = PlayerPrefs.GetInt ("LevelPassed");
 		toggle2.interactable = false;
+		Button_2.GetComponent<Renderer>().enabled = false;
+		toggle3.interactable = false;
+		Button_3.GetComponent<Renderer>().enabled = false;
+		toggle4.interactable = false;
+		Button_4.GetComponent<Renderer>().enabled = false;
+		toggle5.interactable = false;
+		Button_5.GetComponent<Renderer>().enabled = false;
+		toggle6.interactable = false;
+		Button_6.GetComponent<Renderer>().enabled = false;		
 
 
 	switch (levelPassed) 
 		{
 		case 1:
 			toggle2.interactable = true;
-
+			Button_2.GetComponent<Renderer>().enabled = true;
 			break;
 		case 2:
 			toggle2.interactable = true;
-
+			Button_2.GetComponent<Renderer>().enabled = true;
 			toggle3.interactable = true;
-
+			Button_3.GetComponent<Renderer>().enabled = true;
 			break;
 		case 3:
 			toggle2.interactable = true;
-
+			Button_2.GetComponent<Renderer>().enabled = true;
 			toggle3.interactable = true;
-
+			Button_3.GetComponent<Renderer>().enabled = true;
 			toggle4.interactable = true;
-
+			Button_4.GetComponent<Renderer>().enabled = true;
 			break;
 		case 4:
 			toggle2.interactable = true;
-
+			Button_2.GetComponent<Renderer>().enabled = true;
 			toggle3.interactable = true;
-
+			Button_3.GetComponent<Renderer>().enabled = true;
 			toggle4.interactable = true;
-
+			Button_4.GetComponent<Renderer>().enabled = true;
 			toggle5.interactable = true;
-
+			Button_5.GetComponent<Renderer>().enabled = true;
 			break;
 		case 5:
 			toggle2.interactable = true;
-
+			Button_2.GetComponent<Renderer>().enabled = true;
 			toggle3.interactable = true;
-
+			Button_3.GetComponent<Renderer>().enabled = true;
 			toggle4.interactable = true;
-
+			Button_4.GetComponent<Renderer>().enabled = true;
 			toggle5.interactable = true;
-
+			Button_5.GetComponent<Renderer>().enabled = true;
 			toggle6.interactable = true;
-
+			Button_6.GetComponent<Renderer>().enabled = true;
 			break;
 		}
 	}
 	public void resetPlayerPrefs()
 	{
 		toggle2.interactable = false;
-
+		Button_2.GetComponent<Renderer>().enabled = false;
 		toggle3.interactable = false;
-
+		Button_3.GetComponent<Renderer>().enabled = false;
 		toggle4.interactable = false;
-
-		toggle5.interactable = false;	
-
+		Button_4.GetComponent<Renderer>().enabled = false;
+		toggle5.interactable = false;
+		Button_5.GetComponent<Renderer>().enabled = false;
 		toggle6.interactable = false;
-	
+		Button_6.GetComponent<Renderer>().enabled = false;
 		PlayerPrefs.DeleteAll ();
 	}
 }
