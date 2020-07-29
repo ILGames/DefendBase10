112df7d2c66fb2a9abfb5cf0ce59fa22c692bdbf
Marko Andrev <68433525+MarkoAndreev@users.noreply.github.com>
2020-07-29 11:44:48 -0500

    Started on cannon script

---
+++ DefendBase10/Assets/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Cannon : MonoBehaviour
+{
+    public void destroy() 
+    {
+
+        ShipValue valueScript = GetComponent<ShipValue>();
+        ToggleButtonManager buttonValue = GetComponent<ToggleButtonManager>();
+
+        if (valueScript.value == buttonValue.buttonValue)
+        {
+            GameObject destroyShip = GameObject.Find("" + valueScript.value);
+            Destroy(destroyShip);
+        }
+    }
+}
