185685a5a7901ae9f48159b963db499cb170bcc2
LAPTOP-VUSINORT\bobby <bobbylox@importantlittlegames.com>
2020-07-30 10:28:14 -0500

    Main gameplay loop!

--- DefendBase10/Assets/Cannon.cs
+++ DefendBase10/Assets/Cannon.cs
@@ -6,16 +6,35 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+    public Transform spawner;
 
-    public void destroy() 
+    void Update()
     {
+        Fire();
+    }
+
+    public void Fire()
+    {
+        GameObject ctrlPanel = GameObject.Find("ControlPanel");
+        ToggleButtonManager buttonValue = ctrlPanel.GetComponent<ToggleButtonManager>();
+        //Debug.Log("Button ="+buttonValue.buttonValue);
+
+        foreach (Transform child in spawner)
+        {
+            if (child.gameObject.name == "" + buttonValue.buttonValue)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        /*
        	ShipValue valueScript = GetComponent<ShipValue>();
-       	ToggleButtonManager buttonValue = GetComponent<ToggleButtonManager>();
+       	
         GameObject ship = GameObject.Find("" + valueScript.value);
 
         if (valueScript.value == buttonValue.buttonValue)
         {
             Destroy(ship);
         }
+        */
     }
 }
