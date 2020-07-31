ff69fdfe91db8dbd517460fb39058585330ada64
Eealvarez39 <68433382+Eealvarez39@users.noreply.github.com>
2020-07-31 10:35:19 -0500

    ButtonsAppear

--- DefendBase10/Assets/ButtonsOnScreen.cs
+++ DefendBase10/Assets/ButtonsOnScreen.cs
@@ -2,62 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class ButtonsOnScreen : MonoBehaviour
 {
     public Toggley[] buttons;
-    [System.Serializable]
-    public class ButtonsOn
-    {
-    	public int ButtonsAnimated;
-    }
-    public ButtonsOn buttonsOn;
-    GameObject Button_2;
-    GameObject Button_3;
-    GameObject Button_4;
-    GameObject Button_5;
-    GameObject Button_6;
-    GameObject Button_1;
-    void Start()
-    {
-        Button_2 = GameObject.Find("Button_2");
-        Button_3 = GameObject.Find("Button_3");
-        Button_4 = GameObject.Find("Button_4");
-        Button_5 = GameObject.Find("Button_5");
-        Button_6 = GameObject.Find("Button_6");
-    }
+    public int buttonsAnimated;
+
+
 
     void Update()
     {
-	int buttons = 1;
-        
-	/*
-	foreach()
+	Toggley toggley;
+        buttonsAnimated = 0;
+	for(int index = 0; index <buttons.Length; index++)
 	{
-		if (toggley_2.interactable = true)
+		toggley = buttons[index];
+		Debug.Log("Toggley.gameObject" + toggley.gameObject);
+		Toggle toggle = toggley.gameObject.GetComponent<Toggle>();
+		if (toggle.interactable)
 		{
-			buttons +=1;
-		}
-		if (toggley_3.interactable = true)
-		{
-			buttons +=1;
-		}
-		if (toggley_4.interactable = true)
-		{
-			buttons +=1;
-		}
-		if (toggley_5.interactable = true)
-		{
-			buttons +=1;
-		}
-		if (toggley_6.interactable = true)
-		{
-			buttons +=1;
+			buttonsAnimated +=1;
 		}
 
 	}
-	ButtonsAnimated = buttons;
-	*/
-
     }
 }
