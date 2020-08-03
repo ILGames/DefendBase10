d7e216731d80580eae930fa85f64e373926bc431
Eealvarez39 <68433382+Eealvarez39@users.noreply.github.com>
2020-08-03 13:14:28 -0500

    Added two bars which will be coded in depth later

---
+++ DefendBase10/Assets/Scripts/WaveBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveBar : MonoBehaviour
+{
+    private Slider slider;
+    private float waveProgress = 0;
+    public float FillSpeed = 0.5f;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncrementProgress(0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value < waveProgress)
+            slider.value += FillSpeed * Time.deltaTime;
+    }
+    //Adds the progress to the bar
+    public void IncrementProgress(float newprogress)
+    {
+        waveProgress = slider.value + newprogress;
+    }
+}
