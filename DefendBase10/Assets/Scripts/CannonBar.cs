b3512963d6ce006c589fe9442ca8f2bc748c24e6
MarkoAndreev <68433525+MarkoAndreev@users.noreply.github.com>
2020-08-07 10:04:12 -0500

    CannonBar2

---
+++ DefendBase10/Assets/Scripts/CannonBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CannonBar : MonoBehaviour
+{
+    private Slider slider;
+    public float newprogress;
+    
+
+    private void Awake()
+  {
+    slider = gameObject.GetComponent<Slider>();
+  }
+
+  void Update()
+  {
+    newprogress += Time.deltaTime;
+    slider.value = slider.minValue + newprogress;
+  }
+
+  public void ResetBar()
+  {
+    GameObject cannon = GameObject.Find("cannon");
+
+    if (cannon.GetComponent<Cannon>().Fire().IsCalled()) 
+    {
+      newprogress = 0;   
+    }
+  }
+}
