b9c2faf0c0c4063fa7e1a698e77f6a26767678fe
LAPTOP-VUSINORT\bobby <bobbylox@importantlittlegames.com>
2021-11-18 00:33:29 -0600

    Mute toggle works a bit nicer, now

--- DefendBase10/Assets/MuteScript.cs
+++ DefendBase10/Assets/MuteScript.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuteScript : MonoBehaviour
 {
+    public Text text;
     
     public void Mute()
     {
         AudioListener.volume = 0.0f;
+        text.text = "sound\non";
     }
 
     public void UnMute()
     {
         AudioListener.volume = 1.0f;
+        text.text = "sound\noff";
     }
 
     public void ToggleMuted()
